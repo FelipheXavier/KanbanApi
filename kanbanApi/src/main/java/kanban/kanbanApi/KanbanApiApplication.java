cb662501293bd679ee37ba780a15bae34dfc1ab4
Feliphe Xavier <feliphexavier2604@gmail.com>
2024-06-18 12:26:36 -0300

    inicializacao da aplicacao

---
+++ kanbanApi/src/main/java/kanban/kanbanApi/KanbanApiApplication.java
@@ -0,0 +1,13 @@
+package kanban.kanbanApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanApiApplication.class, args);
+	}
+
+}
---
+++ kanbanApi/src/test/java/kanban/kanbanApi/KanbanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kanban.kanbanApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
